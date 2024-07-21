d28fd2a343a83db25325b6f757ecd24a09211d45
simo <svm.zhang@gmail.com>
2024-07-21 22:33:57 +0800

    initial commit; treesitter plugin config

---
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = {
+            "lua",
+            "vim",
+            "javascript",
+            "html",
+            "python",
+            "r",
+            "rust",
+            "bash",
+            "csv",
+            "diff",
+            "markdown",
+            "json",
+            "toml",
+            "xml",
+            "yaml"
+        },
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+      end,
+    }
+}
