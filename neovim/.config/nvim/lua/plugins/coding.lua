e2cfff6402a348828edb7c1a7e6d55a93ac98cf2
simo <svm.zhang@gmail.com>
2024-08-02 10:48:59 +0800

    Add autopairs

--- neovim/.config/nvim/lua/plugins/coding.lua
+++ neovim/.config/nvim/lua/plugins/coding.lua
@@ -119,4 +119,30 @@ return {
 			end, { desc = "Format file or in visual mode" })
 		end,
 	},
+
+	-- autopairs
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			local autopairs = require("nvim-autopairs")
+
+			autopairs.setup({
+				enable_check_bracket_line = false,
+				check_ts = true, -- enable treesitter
+				ts_config = {
+					lua = { "string" },
+					javascript = { "template_string" },
+				},
+			})
+
+			-- If you want insert `(` after select function or method item
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
 }
