86149e0c75dc61a934b3b2f7ea7cc97cf98e34d8
simo <svm.zhang@gmail.com>
2024-08-02 16:56:09 +0800

    Weird formatting, lua vim globals not working anymore

--- neovim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,96 +1,95 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      { "antosha417/nvim-lsp-file-operations", config = true },
-    },
-    config = function()
-      local lspconfig = require("lspconfig")
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			{ "antosha417/nvim-lsp-file-operations", config = true },
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
 
-      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-      local keymap = vim.keymap
-      local opts = { noremap = true, silent = true }
-      local on_attach = function(client, bufnr)
-        opts.buffer = bufnr
+			local keymap = vim.keymap
+			local opts = { noremap = true, silent = true }
+			local on_attach = function(client, bufnr)
+				opts.buffer = bufnr
 
-        opts.desc = "Show LSP reference"
-        keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+				opts.desc = "Show LSP reference"
+				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
 
-        opts.desc = "Show LSP definition"
-        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+				opts.desc = "Show LSP definition"
+				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
 
-        opts.desc = "Show LSP type definition"
-        keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+				opts.desc = "Show LSP type definition"
+				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
-        opts.desc = "Show LSP implementation"
-        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+				opts.desc = "Show LSP implementation"
+				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 
-        opts.desc = "Go to declaration"
-        keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				opts.desc = "Go to declaration"
+				keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
-        opts.desc = "See avaialable code actions"
-        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+				opts.desc = "See avaialable code actions"
+				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
-        opts.desc = "Smart rename"
-        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				opts.desc = "Smart rename"
+				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-        opts.desc = "Show buffer diagnostics"
-        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+				opts.desc = "Show buffer diagnostics"
+				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
-        opts.desc = "Show line diagnostics"
-        keymap.set("n", "<leader>d", vim.diagnostics.open_float, opts)
+				opts.desc = "Show line diagnostics"
+				keymap.set("n", "<leader>d", vim.diagnostics.open_float, opts)
 
-        opts.desc = "Go to previous diagnostics"
-        keymap.set("n", "[d", vim.diagnostics.goto_prev, opts)
+				opts.desc = "Go to previous diagnostics"
+				keymap.set("n", "[d", vim.diagnostics.goto_prev, opts)
 
-        opts.desc = "Go to next diagnostics"
-        keymap.set("n", "]d", vim.diagnostics.goto_next, opts)
+				opts.desc = "Go to next diagnostics"
+				keymap.set("n", "]d", vim.diagnostics.goto_next, opts)
 
-        opts.desc = "Show documentation for what is under cursor"
-        keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				opts.desc = "Show documentation for what is under cursor"
+				keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
-        opts.desc = "Restart LSP"
-        keymap.set("n", "<leader>rs", "<cmd>LspRestart<CR>", opts)
-      end
+				opts.desc = "Restart LSP"
+				keymap.set("n", "<leader>rs", "<cmd>LspRestart<CR>", opts)
+			end
 
-      local capabilities = cmp_nvim_lsp.default_capabilities()
+			local capabilities = cmp_nvim_lsp.default_capabilities()
 
-      local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-      for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-      end
+			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-          Lua = {
-            runtime = { version = "LuaJIT" },
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME
-              }
-            },
-            diagnostics = { globals = {"vim"} }
-          }
-        }
-      })
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME,
+							},
+						},
+						diagnostics = { globals = { "vim" } },
+					},
+				},
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
 
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
 
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
-
-    end,
-  },
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+		end,
+	},
 }
