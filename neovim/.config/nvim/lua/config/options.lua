b9f3fdb8bd3e300f8b164a7bb070bb4d37c92d01
simo <svm.zhang@gmail.com>
2024-08-02 14:52:35 +0800

    initial commmit; global vim options

---
+++ neovim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,27 @@
+local opt = vim.opt
+
+opt.cursorline = true
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 2
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.smartindent = true
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+opt.splitbelow = true
+opt.splitright = true
+
+opt.grepprg = "rg --vimgrep"
+
+opt.backspace = "indent,eol,start"
