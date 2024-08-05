c148f63ce06b2091e4dd7623eb5cafc28da9c1b3
simo <svm.zhang@gmail.com>
2024-08-05 12:45:16 +0800

    Add markdown live preview plugin

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -102,4 +102,25 @@ return {
 			})
 		end,
 	},
+
+	-- markdown live preview
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		-- https://github.com/iamcco/markdown-preview.nvim/issues/690
+		build = function(plugin)
+			if vim.fn.executable("npx") then
+				vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+			else
+				vim.cmd([[Lazy load markdown-preview.nvim]])
+				vim.fn["mkdp#util#install"]()
+			end
+		end,
+		init = function()
+			if vim.fn.executable("npx") then
+				vim.g.mkdp_filetypes = { "markdown" }
+			end
+		end,
+	},
 }
