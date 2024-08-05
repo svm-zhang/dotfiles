afc908bb3af0eba4404e2d6bda55bcbdf8bda368
simo <svm.zhang@gmail.com>
2024-08-05 11:50:51 +0800

    Add keymaps for incremental selection

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,45 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = {
-            "lua",
-            "vim",
-            "javascript",
-            "html",
-            "python",
-            "r",
-            "rust",
-            "bash",
-            "csv",
-            "diff",
-            "markdown",
-            "json",
-            "toml",
-            "xml",
-            "yaml"
-        },
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-      end,
-    }
+		treesitter.setup({
+			-- below 4 lines to set required field
+			-- to avoid missing-field by TSConfig
+			modules = {},
+			sync_install = false,
+			auto_install = false,
+			ignore_install = {},
+			ensure_installed = {
+				"lua",
+				"vim",
+				"javascript",
+				"html",
+				"python",
+				"r",
+				"rust",
+				"bash",
+				"csv",
+				"diff",
+				"markdown",
+				"json",
+				"toml",
+				"xml",
+				"yaml",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
