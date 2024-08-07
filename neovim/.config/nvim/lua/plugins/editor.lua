3b0d1b56593e5c362609cd6fd138392821b0cbe4
simo <svm.zhang@gmail.com>
2024-08-07 12:05:48 +0800

    Load todo-comments plugin when read and open a new buffer

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -134,12 +134,9 @@ return {
 	-- todo comment
 	{
 		"folke/todo-comments.nvim",
+		event = { "BufReadPre", "BufNewFile" },
 		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
+		opts = {},
 	},
 
 	-- vim tmux navigation
