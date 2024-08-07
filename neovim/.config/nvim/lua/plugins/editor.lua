ac51c03eac461629a22589f47ef6289d1181a95c
simo <svm.zhang@gmail.com>
2024-08-07 10:00:31 +0800

    1. Add TODO comment plugin; 2. Update find pattern for messages to be    skipped (not working still)

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -131,6 +131,17 @@ return {
 		config = true,
 	},
 
+	-- todo comment
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+
 	-- vim tmux navigation
 	{
 		"christoomey/vim-tmux-navigator",
@@ -193,8 +204,8 @@ return {
 							{ find = "%d+L, %d+B" },
 							{ find = "; after #%d+" },
 							{ find = "; before #%d+" },
-							{ find = "%d fewer lines" },
-							{ find = "%d more lines" },
+							{ find = "%d fewer line" },
+							{ find = "%d more line" },
 							{ find = "written" },
 							{ find = "Reloading" },
 						},
