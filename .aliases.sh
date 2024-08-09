4a3f891f3f0807c6a8476562eb9ce7e52c7a4538
simo <svm.zhang@gmail.com>
2024-08-09 18:47:13 +0800

    Add a few aliases for git command

--- .aliases.sh
+++ .aliases.sh
@@ -17,8 +17,13 @@ alias du="gdu"
 alias cut="gcut"
 
 # Git
-alias gc="git ci -m"
+alias gc="git commit -m"
+alias gst="git status"
+alias gco="git checkout"
+alias gb="git branch"
 alias gdiff="git diff"
+# copy from https://www.youtube.com/watch?v=HjfEg1pBpjI
+alias glog="git log --graph --topo-order --pretty=\"%w(100,0,6)%C(yellow)%h%C(bold)%C(black)%d %C(cyan)%ar %C(green)%an%n%C(bold)%C(white)%s %N\" --abbrev-commit"
 
 alias r="radian"
 
