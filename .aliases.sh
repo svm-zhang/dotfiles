56a61569bfbcc7f30546c39145612bba5b2ee8e8
simo <svm.zhang@gmail.com>
2024-08-09 18:50:03 +0800

    Update ls aliases

--- .aliases.sh
+++ .aliases.sh
@@ -5,9 +5,8 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 
 # Eza: modern ls
-alias ll='ls -lha'
-alias ls='eza --color=always --long --almost-all --icons=always --no-time --no-user'
-alias lst='eza --color=always --long --tree --almost-all --icons=always --no-time --no-user'
+alias l='eza --color=always --long --almost-all --icons=always --no-time --no-user'
+alias lt='eza --color=always --long --tree --almost-all --icons=always --no-time --no-user'
 
 alias rm="rm -i"
 
