ec17672ac15e0e783a0f25c25fa1731949d63ab8
simo <svm.zhang@gmail.com>
2024-08-10 00:01:21 +0800

    Add toggleterm for send R code to terminal

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -300,4 +300,40 @@ return {
 			})
 		end,
 	},
+
+	-- use toggleterm as primary way to send R code
+	-- to R prompt
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			local toggleterm = require("toggleterm")
+
+			toggleterm.setup({
+				size = 20,
+				direction = "horizontal",
+				-- set shell to be login shell to source .bash_profile
+				-- every time we toggle a terminal
+				shell = "bash -l",
+			})
+
+			local trim_spaces = true
+			local keymap = vim.keymap
+			keymap.set("v", "<leader>s", function()
+				toggleterm.send_lines_to_terminal("visual_lines", trim_spaces, { args = vim.v.count })
+			end, { desc = "Send visual selected lines to terminal" })
+
+			function _G.set_terminal_keymaps()
+				local opts = { buffer = 0 }
+				vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+				vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+				vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+				vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+				vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+			end
+			vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+		end,
+	},
 }
