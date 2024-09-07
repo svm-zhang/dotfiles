8a9b49da6090e8cdbbbb3440019bbcc1c091e637
simo <svm.zhang@gmail.com>
2024-09-07 12:56:30 -0700

    initial commit; separted formatting and linting config from coding.lua

---
+++ neovim/.config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,35 @@
+return {
+	-- formatting
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local conform = require("conform")
+
+			conform.setup({
+				formatters_by_ft = {
+					bash = { "shfmt" },
+					css = { "prettier" },
+					html = { "prettier" },
+					json = { "prettier" },
+					lua = { "stylua" },
+					markdown = { "prettier" },
+					python = function(bufnr)
+						if conform.get_formatter_info("ruff_format", bufnr).available then
+							return { "ruff_organize_imports", "ruff_format" }
+						else
+							return { "isort", "black" }
+						end
+					end,
+					r = { "styler" },
+					yaml = { "prettier" },
+				},
+				format_on_save = {
+					lsp_fallback = true,
+					async = false,
+					timeout = 500,
+				},
+			})
+		end,
+	},
+}
