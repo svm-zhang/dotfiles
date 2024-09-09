ef4bafa5e6e105cd966d17f6a607f20e540cf510
simo <svm.zhang@gmail.com>
2024-09-09 11:48:47 -0700

    Add more plugin integration using catppuccin colorscheme

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -6,11 +6,38 @@ return {
 		priority = 1000,
 		config = function()
 			require("catppuccin").setup({
+				flavour = "macchiato",
+				transparent_background = true,
+				term_colors = true,
 				integrations = {
+					bufferline = true,
 					cmp = true,
+					diffview = true,
 					gitsigns = true,
+					indent_blankline = {
+						enabled = true,
+						scope_color = "lavender",
+					},
+					lsp_trouble = true,
+					mason = true,
+					native_lsp = {
+						enabled = true,
+						underlines = {
+							errors = { "undercurl" },
+							hints = { "undercurl" },
+							warnings = { "undercurl" },
+							information = { "undercurl" },
+						},
+					},
+					neogit = true,
+					neotree = true,
+					noice = true,
+					notify = true,
+					nvim_surround = true,
 					telescope = true,
 					treesitter = true,
+					ufo = true,
+					which_key = true,
 				},
 			})
 
