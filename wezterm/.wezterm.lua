ab0bf2eb3389d8d0b19ee0a609e3800b4e374e74
simo <svm.zhang@gmail.com>
2024-09-09 12:07:35 -0700

    Reformatted with stylua

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -29,38 +29,40 @@ config.audible_bell = "Disabled"
 
 -- tabbar customization
 -- https://github.com/nekowinston/wezterm-bar/tree/main
-wezterm.plugin.require("https://github.com/nekowinston/wezterm-bar").apply_to_config(config, {
-	position = "top",
-	max_width = 32,
-	dividers = "slant_left", -- or "slant_left", "arrows", "rounded", false
-	indicator = {
-		leader = {
-			enabled = true,
-			off = " ",
-			on = " ",
-		},
-		mode = {
-			enabled = true,
-			names = {
-				resize_mode = "RESIZE",
-				copy_mode = "VISUAL",
-				search_mode = "SEARCH",
+wezterm.plugin
+	.require("https://github.com/nekowinston/wezterm-bar")
+	.apply_to_config(config, {
+		position = "top",
+		max_width = 32,
+		dividers = "slant_left", -- or "slant_left", "arrows", "rounded", false
+		indicator = {
+			leader = {
+				enabled = true,
+				off = " ",
+				on = " ",
+			},
+			mode = {
+				enabled = true,
+				names = {
+					resize_mode = "RESIZE",
+					copy_mode = "VISUAL",
+					search_mode = "SEARCH",
+				},
 			},
 		},
-	},
-	tabs = {
-		numerals = "arabic", -- or "roman"
-		pane_count = "superscript", -- or "subscript", false
-		brackets = {
-			active = { "", ":" },
-			inactive = { "", ":" },
+		tabs = {
+			numerals = "arabic", -- or "roman"
+			pane_count = "superscript", -- or "subscript", false
+			brackets = {
+				active = { "", ":" },
+				inactive = { "", ":" },
+			},
 		},
-	},
-	clock = { -- note that this overrides the whole set_right_status
-		enabled = true,
-		format = "%H:%M", -- use https://wezfurlong.org/wezterm/config/lua/wezterm.time/Time/format.html
-	},
-})
+		clock = { -- note that this overrides the whole set_right_status
+			enabled = true,
+			format = "%H:%M", -- use https://wezfurlong.org/wezterm/config/lua/wezterm.time/Time/format.html
+		},
+	})
 
 -- and finally, return the configuration to wezterm
 return config
