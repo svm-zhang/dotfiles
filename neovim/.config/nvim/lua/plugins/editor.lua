377d89fb7b0fa2248c5176aae47eabfcc46de559
simo <svm.zhang@gmail.com>
2024-09-09 13:05:37 -0700

    Customized lualine status components

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -147,6 +147,61 @@ return {
 				options = {
 					theme = "catppuccin",
 				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+					lualine_c = {
+						{
+							"diagnostics",
+							symbols = {
+								error = " ",
+								warn = " ",
+								hint = " ",
+								info = " ",
+							},
+						},
+						{
+							"filetype",
+							icon_only = true,
+							separator = "",
+							padding = { left = 1, right = 0 },
+						},
+					},
+					lualine_x = {
+						{
+							"diff",
+							symbols = {
+								added = " ",
+								modified = " ",
+								removed = " ",
+							},
+							source = function()
+								local gitsigns = vim.b.gitsigns_status_dict
+								if gitsigns then
+									return {
+										added = gitsigns.added,
+										modified = gitsigns.changed,
+										removed = gitsigns.removed,
+									}
+								end
+							end,
+						},
+					},
+					lualine_y = {
+						{
+							"progress",
+							separator = " ",
+							padding = { left = 1, right = 0 },
+						},
+						{ "location", padding = { left = 0, right = 1 } },
+					},
+					lualine_z = {
+						function()
+							return " " .. os.date("%R")
+						end,
+					},
+				},
+				extensions = { "neo-tree", "lazy" },
 			})
 		end,
 	},
