b1eb859fe47c10a5e5802975b8da3ab9e738d60b
simo <svm.zhang@gmail.com>
2024-09-10 07:51:23 -0700

    Fixed formatting with stylua

--- neovim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -36,10 +36,18 @@ return {
 
 			local capabilities = cmp_nvim_lsp.default_capabilities()
 
-			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+			local signs = {
+				Error = " ",
+				Warn = " ",
+				Hint = "󰠠 ",
+				Info = " ",
+			}
 			for type, icon in pairs(signs) do
 				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+				vim.fn.sign_define(
+					hl,
+					{ text = icon, texthl = hl, numhl = "" }
+				)
 			end
 
 			lspconfig.lua_ls.setup({
