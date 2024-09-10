e93b6ca92414aaabf27c455b3f989b605ff0a48b
simo <svm.zhang@gmail.com>
2024-09-10 11:19:46 -0700

    Moved keybind to keymaps.lua; Added options to notify venv activation on python script opens

--- neovim/.config/nvim/lua/plugins/lsp/python.lua
+++ neovim/.config/nvim/lua/plugins/lsp/python.lua
@@ -1,20 +1,21 @@
 return {
 	{
 		"linux-cultist/venv-selector.nvim",
+		branch = "regexp",
 		dependencies = {
 			"neovim/nvim-lspconfig",
 			"nvim-telescope/telescope.nvim",
 			"mfussenegger/nvim-dap-python",
 		},
+		ft = "python",
 		lazy = false,
-		branch = "regexp",
-		config = function()
-			local venv_selector = require("venv-selector")
-			venv_selector.setup({})
-		end,
-		keys = {
-			-- Keymap to open VenvSelector to pick a venv.
-			{ "<leader>vs", "<cmd>VenvSelect<CR>" },
+		opts = {
+			settings = {
+				options = {
+					debug = true,
+					notify_user_on_venv_activation = true,
+				},
+			},
 		},
 	},
 }
