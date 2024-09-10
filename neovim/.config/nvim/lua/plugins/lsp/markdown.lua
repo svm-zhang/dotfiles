64683d846fda1afe323296f98ae2110392250e8a
simo <svm.zhang@gmail.com>
2024-09-10 11:21:12 -0700

    Moved keybind for render-markdown to keymaps.lua; Added markdown-preview plugin from editor.lua

--- neovim/.config/nvim/lua/plugins/lsp/markdown.lua
+++ neovim/.config/nvim/lua/plugins/lsp/markdown.lua
@@ -14,12 +14,35 @@ return {
 					right_pad = 1,
 				},
 			})
-			vim.keymap.set(
-				"n",
-				"<leader>cm",
-				"<cmd>RenderMarkdown toggle<CR>",
-				{ desc = "Toggle to render markdown" }
-			)
+		end,
+	},
+
+	-- markdown live preview
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = {
+			"MarkdownPreviewToggle",
+			"MarkdownPreview",
+			"MarkdownPreviewStop",
+		},
+		ft = { "markdown" },
+		-- https://github.com/iamcco/markdown-preview.nvim/issues/690
+		build = function(plugin)
+			if vim.fn.executable("npx") then
+				vim.cmd(
+					"!cd "
+						.. plugin.dir
+						.. " && cd app && npx --yes yarn install"
+				)
+			else
+				vim.cmd([[Lazy load markdown-preview.nvim]])
+				vim.fn["mkdp#util#install"]()
+			end
+		end,
+		init = function()
+			if vim.fn.executable("npx") then
+				vim.g.mkdp_filetypes = { "markdown" }
+			end
 		end,
 	},
 }
