bf8ecda67c2b5f6d7644f6397826cac3ffa968b9
simo <svm.zhang@gmail.com>
2024-09-14 12:35:10 -0700

    Added url-open plugin to open url directly from neovim

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -131,4 +131,21 @@ return {
 			goto_preview.setup()
 		end,
 	},
+
+	-- url-open.nvim
+	{
+		"sontungexpt/url-open",
+		event = "VeryLazy",
+		cmd = "URLOpenUnderCursor",
+		config = function()
+			local status_ok, url_open = pcall(require, "url-open")
+			if not status_ok then
+				return
+			end
+			url_open.setup({
+				open_app = "default",
+				open_only_when_cursor_on_url = true,
+			})
+		end,
+	},
 }
