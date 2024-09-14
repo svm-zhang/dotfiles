3b9da7035c42434007709f496b53c4e6269e03f6
simo <svm.zhang@gmail.com>
2024-09-13 18:40:58 -0700

    Added config for Obsidian.nvim; the note_id_func has problem

--- neovim/.config/nvim/lua/plugins/obsidian.lua
+++ neovim/.config/nvim/lua/plugins/obsidian.lua
@@ -14,4 +14,61 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
+	config = function()
+		require("obsidian").setup({
+			workspaces = {
+				{
+					name = "notes",
+					path = "$HOME/work/notes",
+				},
+			},
+
+			daily_notes = {
+				folder = "daily",
+				date_format = "%Y-%m-%d",
+				alias_format = "%B %-d, %Y",
+				default_tags = { "daily" },
+				template = "daily.md",
+			},
+
+			mappings = {},
+
+			note_id_func = function(title)
+				local suffix = ""
+				if title ~= nil then
+					suffix = title:gsub("[^A-Za-z0-9]", ""):lower()
+				end
+				return suffix
+			end,
+
+			-- customized from default by not adding title to aliases
+			note_frontmatter_func = function(note)
+				local out =
+					{ id = note.id, aliases = note.aliases, tags = note.tags }
+				if
+					note.metadata ~= nil and not vim.tbl_isempty(note.metadata)
+				then
+					for k, v in pairs(note.metadata) do
+						out[k] = v
+					end
+				end
+				return out
+			end,
+
+			templates = {
+				folder = "templates",
+				date_format = "%Y-%m-%d-%a",
+				time_format = "%H:%M",
+				tags = "",
+				substitutions = {
+					yesterday = function()
+						return os.date("%Y-%m-%d", os.time() - 86400)
+					end,
+					tomorrow = function()
+						return os.date("%Y-%m-%d", os.time() + 86400)
+					end,
+				},
+			},
+		})
+	end,
 }
