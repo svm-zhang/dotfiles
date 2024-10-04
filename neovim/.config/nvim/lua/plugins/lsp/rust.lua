2cb65a194048e702a2adcaf7cd4c88079e3a1a9d
simo <svm.zhang@gmail.com>
2024-10-04 11:11:50 -0700

    initial commit; config for rust language

---
+++ neovim/.config/nvim/lua/plugins/lsp/rust.lua
@@ -0,0 +1,31 @@
+return {
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^5", -- Recommended
+		lazy = false, -- This plugin is already lazy
+	},
+
+	{
+		"saecki/crates.nvim",
+		tag = "stable",
+		dependencies = {
+			"nvim-cmp",
+		},
+		config = function()
+			require("crates").setup({
+				popup = {
+					autofocus = true,
+					border = "rounded",
+				},
+				completion = {
+					cmp = {
+						enabled = true,
+					},
+					crates = {
+						enabled = true,
+					},
+				},
+			})
+		end,
+	},
+}
