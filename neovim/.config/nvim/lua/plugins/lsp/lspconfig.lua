4bae02b606bfc16a775119c9a1fd6cf4b2ec0123
simo <svm.zhang@gmail.com>
2024-11-19 09:22:13 -0800

    Set to use ruff to lint and format python script

--- neovim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -66,7 +66,20 @@ return {
 				capabilities = capabilities,
 			})
 
-			lspconfig.ruff.setup({})
+			lspconfig.pyright.setup({
+				settings = {
+					pyright = {
+						-- Using Ruff's import organizer
+						disableOrganizeImports = true,
+					},
+					python = {
+						analysis = {
+							-- Ignore all files for analysis to exclusively use Ruff for linting
+							ignore = { "*" },
+						},
+					},
+				},
+			})
 
 			lspconfig.r_language_server.setup({
 				filetypes = {
