33cf1710da9863441f8350b756f0226e20bae9a2
simo <3883886+svm-zhang@users.noreply.github.com>
2026-04-15 15:07:59 -0700

    Updated venv selector config to the current recommended default.

--- neovim/.config/nvim/lua/plugins/lsp/python.lua
+++ neovim/.config/nvim/lua/plugins/lsp/python.lua
@@ -1,21 +1,11 @@
 return {
 	{
 		"linux-cultist/venv-selector.nvim",
-		branch = "regexp",
 		dependencies = {
-			"neovim/nvim-lspconfig",
 			"nvim-telescope/telescope.nvim",
-			"mfussenegger/nvim-dap-python",
 		},
 		ft = "python",
 		lazy = false,
-		opts = {
-			settings = {
-				options = {
-					debug = true,
-					notify_user_on_venv_activation = true,
-				},
-			},
-		},
+		opts = {},
 	},
 }
